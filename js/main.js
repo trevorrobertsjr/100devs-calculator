d48be6e697cfad36bf63e855e82c34b6feb98ef1
Trevor Roberts Jr <Trevor.Roberts.Jr@gmail.com>
2022-08-07 08:28:08 -0400

    added logic to get button inputs

--- js/main.js
+++ js/main.js
@@ -4,9 +4,20 @@
 
 
 // document.getElementsByName("td").addEventListener("click", displayCalculation);
-document.querySelector("td").addEventListener("click", displayCalculation);
+// document.querySelector("td").addEventListener("click", displayCalculation);
 
-function displayCalculation() {
-    alert("button clicked!");
+// document.querySelectorAll("td").forEach(e => e.addEventListener("click", displayCalculation))
+
+// function displayCalculation() {
+//     alert("button clicked!");
+
+// }
+
+// anonymous function used so that I can specify a function with a parameter.
+// event listeners do not allow for parameters.
+document.querySelectorAll("td").forEach(e => e.addEventListener("click", () => { displayCalculation(e.innerText) }))
+
+function displayCalculation(input) {
+    alert(`${input}`);
 
 }
