09352f7e9ea1f3cf39345bd25516c3bd50593285
Trevor Roberts Jr <Trevor.Roberts.Jr@gmail.com>
2022-08-07 07:57:00 -0400

    begin calculator logic. basic interface done. TBD: implement calculator logic; improve frontend

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 
+// an array for inputs. As each button is clicked append to array. When = is hit, complete the equation.
+// stretch goal: every time an operator is clicked, show the intermediate value.
+
+
+// document.getElementsByName("td").addEventListener("click", displayCalculation);
+document.querySelector("td").addEventListener("click", displayCalculation);
+
+function displayCalculation() {
+    alert("button clicked!");
+
+}
